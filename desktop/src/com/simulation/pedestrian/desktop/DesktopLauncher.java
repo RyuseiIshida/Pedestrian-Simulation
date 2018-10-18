bc70d0a19b39c6c9bd836cc0252ec9c4dd342ecc
Ryusei Ishida <serys9469.japan@gmail.com>
2018-10-18 12:10:06 +0900

    :octocat: Initial commit

---
+++ desktop/src/com/simulation/pedestrian/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.simulation.pedestrian.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.simulation.pedestrian.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
