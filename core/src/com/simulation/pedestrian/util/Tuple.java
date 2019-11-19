facbaab1b6c009cec50e123e6a149a4e920ebc09
RyuseiIshida <serys9469.japan@gmail.com>
2019-11-19 14:58:28 +0900

    :art: supporting generics

--- core/src/com/simulation/pedestrian/util/Tuple.java
+++ core/src/com/simulation/pedestrian/util/Tuple.java
@@ -1,37 +1,24 @@
 package com.simulation.pedestrian.util;
 
-public class Tuple {
-    public int t1;
-    public int t2;
+public class Tuple<T> {
+    public final T valueA;
+    public final T valueB;
 
-    public Tuple() {
-        this.t1 = 0;
-        this.t1 = 0;
+    public Tuple(T valueA, T valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
     }
 
-    public Tuple(int t1, int t2) {
-        this.t1 = t1;
-        this.t2 = t2;
+    public T getValueA() {
+        return valueA;
     }
 
-    public void setT1(int t1) {
-        this.t1 = t1;
-    }
-
-    public void setT2(int t2) {
-        this.t2 = t2;
-    }
-
-    public int getT1() {
-        return t1;
-    }
-
-    public int getT2() {
-        return t2;
+    public T getValueB() {
+        return valueB;
     }
 
     @Override
     public String toString() {
-        return "[" + t1 + " ," + t2 + "]";
+        return "[" + valueA + " ," + valueB + "]";
     }
 }
