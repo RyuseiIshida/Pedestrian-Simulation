59a366af62c926867d79c72efad4e1074071ada8
serys9469.japan@gmail.com <19960409r>
2020-03-10 03:36:04 +0900

    :recycle: refactor

--- core/src/com/github/ryuseiishida/pedestrian_simulation/util/Parameter.java
+++ core/src/com/github/ryuseiishida/pedestrian_simulation/util/Parameter.java
@@ -20,7 +20,7 @@ public class Parameter {
     /**
      * シミュレーションループ回数
      */
-    public static int LOOP_NUM = 2;
+    public static int LOOP_NUM = 1;
 
     /**
      * シミュレーション終了ステップ
@@ -30,7 +30,7 @@ public class Parameter {
     /**
      * ログの記録
      */
-    public static boolean IS_WRITE_LOG = true;
+    public static boolean IS_WRITE_LOG = false;
     
     public static boolean IS_WRITE_LOG_AGENT = false;
     
@@ -56,10 +56,6 @@ public class Parameter {
     public static ArrayList<Goal> GOALS = new ArrayList<>(Arrays.asList(
             new Goal(0, 200, 200, 200 + 200),
                         new Goal(SCALE.x - 200, SCALE.y - 1400, 200, 200 + 200)
-//            new Goal(8900, 4000, 100, 2000)
-//            new Goal(-1000, -4000, 0, 0)
-//            new Goal(9000 + 200, SCALE.y / 2 + 3000, 0, 0)
-//            new Goal(9000 + 200, SCALE.y / 2 - 5000, 100, 2000)
     ));
     public static int CELL_INTERVAL = 10;
 
@@ -70,30 +66,21 @@ public class Parameter {
     public static CellsMap ENV_CELLS_MAP = new CellsMap(SCALE, CELL_INTERVAL);
 
     public static ArrayList<BoxLine> Boxes = new ArrayList<>(Arrays.asList(
-//            // 左上
-//            new BoxLine(1200, 6200, 4500, 8000, cellsMap),
-//            //右上
-//            new BoxLine(5700, 6200, 9000, 8000, cellsMap),
-//            // 左下
-//            new BoxLine(1200, 1200, 4500, 3000, cellsMap),
-//            //右下
-//            new BoxLine(5700, 1200, 9000, 3000, cellsMap),
-//            //中央左
-//            new BoxLine(1200, 3700, 4500, 5500, cellsMap),
-//            //中央右
-//            new BoxLine(5700, 3700, 9000, 5500, cellsMap)
+            // 左上
+            new BoxLine(1200, 6200, 4500, 8000, ENV_CELLS_MAP),
+            //右上
+            new BoxLine(5700, 6200, 9000, 8000, ENV_CELLS_MAP),
+            // 左下
+            new BoxLine(1200, 1200, 4500, 3000, ENV_CELLS_MAP),
+            //右下
+            new BoxLine(5700, 1200, 9000, 3000, ENV_CELLS_MAP),
+            //中央左
+            new BoxLine(1200, 3700, 4500, 5500, ENV_CELLS_MAP),
+            //中央右
+            new BoxLine(5700, 3700, 9000, 5500, ENV_CELLS_MAP)
     ));
 
     public static ArrayList<Obstacle> OBSTACLES = new ArrayList<>(Arrays.asList(
-//            //left wall line
-//            new Line(0 - CELL_INTERVAL, 0, 0, SCALE.y, cellsMap),
-//            //right wall line
-//            new Line(SCALE.x, 0, SCALE.x, SCALE.y, cellsMap),
-//            // bottom wall line
-//            new Line(0 - CELL_INTERVAL, 0, SCALE.x, 0, cellsMap),
-//            // top wall line
-//            new Line(0, SCALE.y, SCALE.x, SCALE.y, cellsMap),
-
             //left wall line
             new Line(200, 600, 200, SCALE.y - 1000, ENV_CELLS_MAP),
             //right wall line
@@ -103,63 +90,6 @@ public class Parameter {
             new Line(200, 200, SCALE.x - 200, 200, ENV_CELLS_MAP),
             // top wall line
             new Line(200, SCALE.y - 1000, SCALE.x - 200, SCALE.y - 1000, ENV_CELLS_MAP)
-//
-//            // 左下
-//            // left
-//            new Line(1200, 1200, 1200, 3000, cellsMap),
-//            //right
-//            new Line(4000, 1200, 4000, 3000, cellsMap),
-//            //bottom
-//            new Line(1200, 1200, 4000, 1200, cellsMap),
-//            //top
-//            new Line(1200, 3000, 4000, 3000, cellsMap),
-//
-//            // 左上
-//            // left
-//            new Line(1200, 6200, 1200, 8000, cellsMap),
-//            //right
-//            new Line(4000, 6200, 4000, 8000, cellsMap),
-//            //bottom
-//            new Line(1200, 6200, 4000, 6200, cellsMap),
-//            //top
-//            new Line(1200, 8000, 4000, 8000, cellsMap),
-//
-//            //右上
-//            new Line(6200, 6200, 6200, 8000, cellsMap),
-//            //right
-//            new Line(9000, 6200, 9000, 8000, cellsMap),
-//            //bottom
-//            new Line(6200, 6200, 9000, 6200, cellsMap),
-//            //top
-//            new Line(6200, 8000, 9000, 8000, cellsMap),
-//
-//
-//            //右下
-//            new Line(6200, 1200, 6200, 3000, cellsMap),
-//            //right
-//            new Line(9000, 1200, 9000, 3000, cellsMap),
-//            //bottom
-//            new Line(6200, 1200, 9000, 1200, cellsMap),
-//            //top
-//            new Line(6200, 3000, 9000, 3000, cellsMap),
-//
-//            //中央右
-//            new Line(6200, 3700, 6200, 5500, cellsMap),
-//            //right
-//            new Line(9000, 3700, 9000, 5500, cellsMap),
-//            //bottom
-//            new Line(6200, 3700, 9000, 3700, cellsMap),
-//            //top
-//            new Line(6200, 5500, 9000, 5500, cellsMap),
-//
-//            //中央左
-//            new Line(1200, 3700, 1200, 5500, cellsMap),
-//            //right
-//            new Line(4000, 3700, 4000, 5500, cellsMap),
-//            //bottom
-//            new Line(1200, 3700, 4000, 3700, cellsMap),
-//            //top
-//            new Line(1200, 5500, 4000, 5500, cellsMap)
     ));
 
     public static Line experienceOLINE = new Line(0, 0, 0, 0, ENV_CELLS_MAP);
@@ -172,7 +102,7 @@ public class Parameter {
     public static float POTENTIAL_DELTA = 1f;
 
     //agent
-    public static int INIT_AGENT_NUM = 30;
+    public static int INIT_AGENT_NUM = 100;
     public static int GOAL_AGENT_NUM = 30;
     public static Tuple<Float> INIT_RANDOM_X = new Tuple<>(300f, SCALE.x - 300);
     public static Tuple<Float> INIT_RANDOM_Y = new Tuple<>(300f, SCALE.y - 1100);
